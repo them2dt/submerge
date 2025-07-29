d24e6a0b2ea82dbdf65262914b1e46cab06ea9d7
Maru <mtcreatives@hotmail.com>
2025-07-29 10:29:00 +0200

    update images, and add seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,10 @@ const satoshi = localFont({
 export const metadata: Metadata = {
   title: "Submerge - Subscription Calculator",
   description: "Calculate and visualize your subscription spending.",
+  icons: "/icon.png",
+  openGraph: {
+    images: "/social-image.png",
+  }
 };
 
 export default function RootLayout({
